d9c39983cb3d381759ceafeb593634e4ebace485
jihao <1143219312@qq.com>
2024-12-27 17:22:36 +0800

    yaml工具兼容

--- cookie_tools/tools/reader.py
+++ cookie_tools/tools/reader.py
@@ -4,14 +4,19 @@ import os
 
 # yaml读取
 def yaml_reader(file_path):
-
     try:
         with open(file=file_path, encoding='utf-8') as f:
             temp_data = yaml.safe_load(f)
             def inner(func):
-                def outer(self, *args, **kwargs):
-                    func(self, temp_data, *args, **kwargs)
-                return outer
+                # 兼容不包含self函数的调用
+                if func.__code__.co_varnames[:func.__code__.co_argcount][0] == 'self':
+                    def outer_with_self(self, *args, **kwargs):
+                        func(self, temp_data, *args, **kwargs)
+                    return outer_with_self
+                else:
+                    def outer(*args, **kwargs):
+                        func(temp_data, *args, **kwargs)
+                    return outer
             return inner
     except IOError as e:
         print("文件读取出错{}".format(e))
